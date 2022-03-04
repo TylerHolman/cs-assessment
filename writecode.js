693d701349841e236792fa1a0d07576025c0e6f4
Tyler Holman <holman.tyken@yahoo.com>
2022-03-04 13:16:50 -0600

    initial commit

---
+++ writecode.js
@@ -0,0 +1,18 @@
+// Write a function that takes in an array of numbers. The function should return True if any two numberss in list sum to 0, and false otherwise.
+nums = []
+const addToZero = (nums) => {
+
+}
+
+// Write a function that takes in a single word, as a string. It should return True if that word contains only unique characters. Return False otherwise.
+
+
+
+
+// A pangram is a sentence that contains all the letters of the English alphabet at least once, like “The quick brown fox jumps over the lazy dog.”
+
+// Write a function to check a sentence to see if it is a pangram or not.
+
+
+
+// Write a function, find_longest_word, that takes a list of words and returns the length of the longest one.
