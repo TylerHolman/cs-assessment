1afe967c723f73ab96897cbfc09b18fc3c3d9174
Tyler Holman <holman.tyken@yahoo.com>
2022-03-05 15:25:40 -0600

    final commit

--- writecode.js
+++ writecode.js
@@ -2,17 +2,17 @@
 
 const addToZero = (num) => {
     for(let i = 0; i < num.length; i++){
-        for(let j = 1; j < num.length; j++){
-            if(num[i] + num[j] === 0){
-                
+        for(let j =1; j < num.length; j++){
+            if(num[i] + num[j] === 0){  
                 return true
-            }else {
-                return false
             }
         }
     }
+
+    return false
 }
-console.log(addToZero([1,1]))
+console.log(addToZero([1,1,5,8, 5, 1,8]))
+//run time complexity O(n^2)
 
 // Write a function that takes in a single word, as a string. It should return True if that word contains only unique characters. Return False otherwise.
 
@@ -29,7 +29,7 @@ const hasUniqChar = (word) => {
 console.log(hasUniqChar("Monday"))
 
 
-
+//run time complexity O(n^2)
 
 // A pangram is a sentence that contains all the letters of the English alphabet at least once, like “The quick brown fox jumps over the lazy dog.”
 
@@ -47,4 +47,21 @@ const panContain = (string) => {
 }
 console.log(panContain(`The quick brown fox jumps over the lazy dog.`))
 
+//run time complexity O(n)
+
 // Write a function, find_longest_word, that takes a list of words and returns the length of the longest one
+
+const findLongestWord = (word) => {
+    let results = 0
+
+    for(let i = 0; i < word.length; i++){
+        if(word[i].length > results){
+            results = word[i].length
+        }
+    }
+    return results
+}
+
+console.log(findLongestWord(['big', 'small', 'orange', 'it', 'blankenship']))
+
+//time complexity O(n)
